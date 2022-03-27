58e5eb870f032d25b0948c46a0ba6340b0a33c73
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-03-27 13:56:52 -0500

    Clicking an active card changes the scene

---
+++ Assets/Scripts/AvatarCards.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AvatarCards : MonoBehaviour
+{
+
+    public Animator animator;
+    private bool mouseOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.SetBool("Mouse Over", false);
+        mouseOver = false;
+    }
+
+
+    void OnMouseOver()
+    {
+        animator.SetBool("Mouse Over", true);
+        mouseOver = true;
+    }
+
+    void OnMouseExit()
+    {
+        animator.SetBool("Mouse Over", false);
+        mouseOver = false;
+    }
+
+    void Update()
+    {
+        if(mouseOver && Input.GetMouseButtonDown(0) && ((gameObject.transform.Find("BlankCard")) == null))
+        {
+            SceneManager.LoadScene("OpeningScene");
+        }
+    }
+}
