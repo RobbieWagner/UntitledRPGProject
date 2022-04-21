53954f30f75b068f82784e25a7dd1908dec4a0ed
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-04-21 17:20:07 -0500

    Added Clothing

--- Assets/Scripts/CharacterCustomizer.cs
+++ Assets/Scripts/CharacterCustomizer.cs
@@ -9,10 +9,27 @@ public class CharacterCustomizer : MonoBehaviour
     public InitializeCharacter characterInitialization;
 
     public Sprite[] hairstyles;
+    public Sprite[] tops;
+    public Sprite[] bottoms;
+    public Sprite[] shoes;
 
     public SpriteRenderer hair;
     public int hairIndex;
     bool changedHairstyle;
+
+    public SpriteRenderer top;
+    public int topIndex;
+    bool changedTop;
+
+    public SpriteRenderer bottom;
+    public int bottomIndex;
+    bool changedBottom;
+
+    public SpriteRenderer shoe;
+    public int shoeIndex;
+    bool changedShoes;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -31,7 +48,25 @@ public class CharacterCustomizer : MonoBehaviour
             hairIndex = characterInitialization.hairIndex;
         }
 
+        if(topIndex == -1)
+        {
+            topIndex = characterInitialization.topIndex;
+        }
+
+        if(bottomIndex == -1)
+        {
+            bottomIndex = characterInitialization.bottomIndex;
+        }
+
+        if(shoeIndex == -1)
+        {
+            shoeIndex = characterInitialization.shoeIndex;
+        }
+
         if(changedHairstyle) hair.sprite = hairstyles[hairIndex];
+        if(changedTop) top.sprite = tops[topIndex];
+        if(changedBottom) bottom.sprite = bottoms[bottomIndex];
+        if(changedShoes) shoe.sprite = shoes[shoeIndex];
     }
 
     public void HairstyleChange(bool up)
@@ -47,6 +82,57 @@ public class CharacterCustomizer : MonoBehaviour
             if(hairIndex == 0) hairIndex = hairstyles.Length-1;
             else hairIndex--;
             changedHairstyle = true;
+        }
+
+    }
+
+    public void TopChange(bool up)
+    {
+        if(up)
+        {
+            if(topIndex == tops.Length-1) topIndex = 0;
+            else topIndex ++;
+            changedTop = true;
+        }
+        else
+        {
+            if(topIndex == 0) topIndex = tops.Length-1;
+            else topIndex--;
+            changedTop = true;
+        }
+
+    }
+
+    public void BottomChange(bool up)
+    {
+        if(up)
+        {
+            if(bottomIndex == bottoms.Length-1) bottomIndex = 0;
+            else bottomIndex ++;
+            changedBottom = true;
+        }
+        else
+        {
+            if(bottomIndex == 0) bottomIndex = bottoms.Length-1;
+            else bottomIndex--;
+            changedBottom = true;
+        }
+
+    }
+
+    public void ShoeChange(bool up)
+    {
+        if(up)
+        {
+            if(shoeIndex == shoes.Length-1) shoeIndex = 0;
+            else shoeIndex ++;
+            changedShoes = true;
+        }
+        else
+        {
+            if(shoeIndex == 0) shoeIndex = shoes.Length-1;
+            else shoeIndex--;
+            changedShoes = true;
         }
 
     }
--- Assets/Scripts/InitializeCharacter.cs
+++ Assets/Scripts/InitializeCharacter.cs
@@ -16,11 +16,23 @@ public class InitializeCharacter : MonoBehaviour
     public SpriteRenderer hair;
     public int hairIndex;
 
+    public SpriteRenderer top;
+    public int topIndex;
+
+    public SpriteRenderer bottom;
+    public int bottomIndex;
+
+    public SpriteRenderer shoe;
+    public int shoeIndex;
+
     // Start is called before the first frame update
     void Start()
     {
         typePassed = false;
         hairIndex = Random.Range(0, characterCustomizer.hairstyles.Length);
+        topIndex = Random.Range(0, characterCustomizer.tops.Length);
+        bottomIndex = Random.Range(0, characterCustomizer.bottoms.Length);
+        shoeIndex = Random.Range(0, characterCustomizer.shoes.Length);
     }
 
     // Update is called once per frame
@@ -30,6 +42,9 @@ public class InitializeCharacter : MonoBehaviour
         {
             player.GetComponent<SpriteRenderer>().sprite = baseSprites[elementType];
             hair.sprite = characterCustomizer.hairstyles[hairIndex];
+            top.sprite = characterCustomizer.tops[topIndex];
+            bottom.sprite = characterCustomizer.bottoms[bottomIndex];
+            shoe.sprite = characterCustomizer.shoes[shoeIndex];
             typePassed = false;
         }
     }
